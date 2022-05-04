e404a780462306b9016930475829319e5dac97c6
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-05-04 18:56:14 -0500

    Added functioning get by 1

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
   try {
     const category = await Category.findAll({
-      include: [ { model: Product, as: 'products' }]
+      include: [ { model: Product, as: 'products' } ]
     });
     res.status(200).json(category);
   } catch (err) {
@@ -19,6 +19,20 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      // JOIN with locations, using the Trip through table
+      include: [ { model: Product, as: 'products' } ]
+    });
+    if (!category) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', async (req, res) => {
