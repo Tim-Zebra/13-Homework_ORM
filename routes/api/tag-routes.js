2d3def3e9f97064108e0063c6e8bf930b61fb55c
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-05-05 17:02:13 -0500

    post route working for tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,8 +34,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
   // create a new tag
+router.post('/', async (req, res) => {
+  try {
+    const tag = await Tag.create(req.body);
+    res.status(200).json(tag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
